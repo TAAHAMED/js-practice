4a4bfa89a201925eeb217ba099905f5f6a3a08d9
TAAHAMED <talib.ahamed@capgemini.com>
2025-04-26 08:58:57 +0000

    operations on dataTypes

---
+++ 01_basic/operations.js
@@ -0,0 +1,47 @@
+let value = 3
+let negValue = - value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Talib"
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3+4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let counter = 100
+// counter++
+++counter
+console.log(counter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
