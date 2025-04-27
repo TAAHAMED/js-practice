696efff6943771ffbdf4a6a30a3e7065805fc9e1
TAAHAMED <talib.ahamed@capgemini.com>
2025-04-27 05:54:48 +0000

    date functions added

---
+++ 01_basic/date.js
@@ -0,0 +1,14 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 5, 23)
+console.log(myCreatedDate.toDateString());
+
+
+
+
