7af5fbaca64d88838fea54fa69d062daf9deb234
TAAHAMED <talib.ahamed@capgemini.com>
2025-04-27 05:35:53 +0000

    number and math function added

---
+++ 01_basic/number.js
@@ -0,0 +1,18 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);     
+
+
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
